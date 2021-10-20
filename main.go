27575ce5a5b91faaaf9d380b317e1cdf6f4cccc0
UmbrellaJo <81673211@qq.com>
2021-10-20 15:54:17 +0800

    demo

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	log.Println("abc")
+	INFO.Println("hahaha")
+}
+
+var (
+	INFO *log.Logger
+)
+
+func init() {
+	filename := "info.log"
+	logFile, err := os.Create(filename)
+	if err != nil {
+		log.Fatalln("create log file err", err)
+	}
+	INFO = log.New(logFile, "[INFO]", log.Ldate|log.Ltime|log.Llongfile)
 }
